49eabdceb2da516115fe0b0fcae98be5c437b913
slavyakhin <slavyakhin@gmail.com>
2024-09-14 17:46:42 +0300

    Lab n1. Create entry point and the function.

---
+++ Lab_n1/function.hpp
@@ -0,0 +1,3 @@
+#include <string>
+
+int function(std::string str);
---
+++ Lab_n1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "function.hpp"
+
+// Вариант 4
+// Функция возвращает количество гласных букв в заданной строке (на английском).
+// Входная строка будет состоять только из строчных букв и/или пробелов.
+
+int main(int argc, char* argv[]) {
+
+    std::string str;
+    std::getline(std::cin, str);            // line may consist only of spaces
+    std::cout << function(str) << std::endl;
+
+    return 0;
+}
